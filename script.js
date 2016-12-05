bc3a00afbd9532ed293bd2821c05edf37c033535
Greg Bowler <greg.bowler@g105b.com>
2016-12-05 21:45:03 +0000

    MVP.

---
+++ script.js
@@ -0,0 +1,38 @@
+pwd.addEventListener("submit", generate);
+pwd["output"].addEventListener("click", copy);
+
+function generate(e) {
+	e.preventDefault();
+	var options = {
+		counter: 1,
+		length: 16,
+		lowercase: true,
+		uppercase: true,
+		numbers: true,
+		// symbols: false,
+		template: 'vcVCns'
+	};
+
+	LessPass.encryptLogin(pwd["username"].value, pwd["master"].value)
+	.then(function(response) {
+		LessPass.renderPassword(
+			response,
+			pwd["service"].toLowerCase().replace(/\s/g, ''),
+			options
+		)
+		.then(function(output) {
+			pwd["output"].innerHTML = output;
+		});
+	});
+}
+
+function copy(e) {
+	this.select();
+	if(document.execCommand("copy")) {
+		document.body.classList.add("coppied");
+
+		setTimeout(function() {
+			document.body.classList.remove("coppied");
+		}, 1000);
+	}
+}
